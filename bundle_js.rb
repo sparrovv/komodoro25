2d07d3dffded000731f67b0d0fbb3a6efeef1b78
Michal Wrobel <sparrovv@gmail.com>
2012-07-22 20:43:33 +0200

    Add safe flag

--- bundle_js.rb
+++ bundle_js.rb
@@ -3,12 +3,16 @@ input_files = ['js/init.js', 'js/templates.js', 'js/local_storage.js', 'js/timer
 
 bundle_file = File.new 'js/komodoro_bundle.js' , "w"
 
-bundle_file << "try {\n"
+safe_version = ARGV[0] and ARGV[0] == 'safe'
+
+bundle_file << "try {\n" if safe_version
+
 input_files.each do |file|
   file_content = File.open(file).read
 
   bundle_file << file_content
 end
-bundle_file << "} catch(e) { console.log(e); }\n"
+
+bundle_file << "} catch(e) { console.log(e); }\n" if safe_version
 
 puts "js/komodoro_bundle.js generated"
